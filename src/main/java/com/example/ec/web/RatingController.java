3eca11ce0dcc8ddf39e8549cd892849eb9bf8ee4
Ishan Praneethan <praneeth.ishan@gmail.com>
2020-06-28 22:36:29 +0530

    Adding swagger api doc support

--- src/main/java/com/example/ec/web/RatingController.java
+++ src/main/java/com/example/ec/web/RatingController.java
@@ -1,6 +1,11 @@
 package com.example.ec.web;
 
 import com.example.ec.service.TourRatingService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.ApiResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +19,7 @@ import java.util.NoSuchElementException;
 /**
  * Created by Mary Ellen Bowman
  */
+@Api(description = "API to just pull ratings")
 @RestController
 @RequestMapping(path = "/ratings")
 public class RatingController {
@@ -29,13 +35,17 @@ public class RatingController {
     }
 
     @GetMapping
+    @ApiOperation(value = "Find all ratings")
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "OK") })
     public List<RatingDto> getAll() {
         LOGGER.info("GET /ratings");
         return assembler.toResources(tourRatingService.lookupAll());
     }
 
     @GetMapping("/{id}")
-    public RatingDto getRating(@PathVariable("id") Integer id) {
+    @ApiOperation(value = "Find ratings by id")
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "OK"), @ApiResponse(code = 404, message = "Rating not found") })
+    public RatingDto getRating(@ApiParam(value = "rating identifier") @PathVariable("id") Integer id) {
         LOGGER.info("GET /ratings/{id}", id);
         return assembler.toResource(tourRatingService.lookupRatingById(id)
                 .orElseThrow(() -> new NoSuchElementException("Rating " + id + " not found"))
